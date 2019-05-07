fca7e5c42286767cf33ad7958cc82f9b8ae9031a
CDTruong <dungtr95@gmail.com>
2019-05-07 14:33:34 +0700

    update real-time function for machine-monitor

--- src/app/pages/dashboard/machines-monitor/machines-monitor.component.ts
+++ src/app/pages/dashboard/machines-monitor/machines-monitor.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Machine } from '../../@model/machine';
 import { MachineService } from '../../@service/machine.service';
 import { takeWhile } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { RefreshService } from '../../../@service/refresh.service';
 
 @Component({
   selector: 'ngx-machines-monitor',
@@ -13,8 +15,13 @@ export class MachinesMonitorComponent implements OnInit, OnDestroy {
   private alive = true;
   public machines: Machine[];
 
+  private timeRefreshSubscription: Subscription;
+  public selectedItem: string;
+  public speedVal: number = 5000;
+  
   constructor(
     private machineService: MachineService,
+    private refreshService: RefreshService,
   ) { }
 
   ngOnInit() {
@@ -25,10 +32,24 @@ export class MachinesMonitorComponent implements OnInit, OnDestroy {
     .subscribe(
       machines => this.machines = machines,
     );
+    this.updateMachineStatus();
   }
 
   ngOnDestroy() {
     this.alive = false;
+  }
+
+  updateMachineStatus() {
+    this.timeRefreshSubscription = this.refreshService.withRefresh()
+      .subscribe(
+        () => {
+          this.machineService.getMachines()
+            .subscribe((data) => {
+              // console.log(this.speedVal);
+              this.machines = data;
+            });
+        }
+      );
   }
 
 }
